6a36ca14e1ba55089ddb103297f24af9e9be8dba
L3i <118181023+L3iCodes@users.noreply.github.com>
2025-08-22 17:06:43 +0800

    Minor package change and add login function
    - Change mysql to mysql/promise for async query.
    
    - Add login function
    - Create acccess and refresh token
    - Add refresh function

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -13,7 +14,6 @@ const pool = mysql.createPool({
     connectionLimit: 10
 });
 
-
 // Insert User in DB
 export const sign_up = async (req, res) =>{
     const {username, password} = req.body;
@@ -23,31 +23,117 @@ export const sign_up = async (req, res) =>{
         const hashed_password = await bcrypt.hash(password, 10);
         console.log(`Creating New User: \nUsername: ${username} \nHashedPaswword: ${hashed_password}`)
 
-        // username has a UNIQUE Constraint
-        pool.query(
+        const [result] = await pool.query(
             `INSERT INTO practice_database.users (username, hashed_password) VALUES (?, ?)`,
-            [username.toLowerCase(), hashed_password],
-            (err, result) => {
-                if(err){
-                    if (err.code === "ER_DUP_ENTRY") {
-                        // Duplicate username case
-                        return res.status(409).json({
-                            success: false,
-                            message: "Username already exists"
-                        });
-                    }
-                    // Other DB errors
-                    return res.status(500).json({
-                        success: false,
-                        message: "Database error",
-                    });
-                }
-                
-                return res.status(201).json({success: true, message: result})
-            }
-        )
+            [username.toLowerCase(), hashed_password]
+        );
+
+        return res.status(201).json({success: true, message: "Succesfully created user", result})
 
     }catch(error){
-        return res.status(401).json({success: false, message: 'Failed to add user' + error})
+        console.log(error.code)
+        if(error.code === 'ER_DUP_ENTRY'){
+            return res.status(401).json({success: false, message: 'Username already used'})
+        
+        }else{
+            return res.status(500).json({success: false, message: 'Failed to add user', error})
+        }
     }
 }
+
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    console.log('Searching for USER: ' + username)
+    
+    try{
+        const [row] = await pool.query(
+            `SELECT user_id, username, hashed_password FROM practice_database.users WHERE username = ?`,
+            [ username ],
+        );
+
+        // Check if user exists
+        if(row.length < 1){
+            return res.status(401).json({success: false, message: 'User does not exists'})
+        };
+
+        // Compare input password to hashed password
+        const match = await bcrypt.compare(password, row[0].hashed_password);
+        if(!match) {
+            return res.status(401).json({success: false, message: 'Incorrect password or username'})
+        };
+
+        // Create ACCESS TOKEN
+        const accessToken = jwt.sign(
+            {user_id: row[0].user_id, username: row[0].username,}, 
+            process.env.SECRET, 
+            {expiresIn: '1h'}
+        );
+
+        // Create REFRESH TOKEN 
+        const refreshToken = jwt.sign(
+            {user_id: row[0].user_id, username: row[0].username,}, 
+            process.env.SECRET, 
+            {expiresIn: '7d'}
+        );
+        
+        // Store REFRESH TOKEN in HttpOnly Cookie
+        res.cookie("jwt", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days 24hr 60min 60sec 1000ms
+        })
+
+        return res.status(401).json({
+            success: true, 
+            message: 'Login complete', 
+            credentials: {
+                user_id: row[0].user_id,
+                username: row[0].username,
+                token: accessToken
+            }
+        });
+        
+    }catch(error){
+        return res.status(401).json({success: false, message: 'Login failed', error})
+    }
+}
+
+export const refresh = (req, res) => {
+    const cookies = req.cookies;
+
+    if(!cookies?.jwt){
+        return res.status(401).json({success:false, message: 'No refresh token found'});
+    }
+
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(refreshToken, process.env.SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({success:false, message: 'Invalid refresh token found'});
+
+        // Create new access token
+        const accessToken = jwt.sign(
+            {user_id: decoded.user_id, username: decoded.username},
+            process.env.SECRET,
+            { expiresIn: '1h' }
+        );
+
+        return res.json({ accessToken })
+    });
+};
+
+
+export const authenticateJWT = (req, res, next) => {
+    const authHeader = res.header['authorization'];
+    if (!authHeader) return res.status(401).json({success:false, message: 'No token found'});
+
+
+    const token = authHeader.split(" ")[1]; //Bearer <token>
+    jwt.verify(token, process.env.SECRET, (err, user) => {
+        if (err) res.status(401).json({success:false, message: 'Invalid token'});
+
+        req.user = user;
+        next();
+
+    });
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { sign_up } from '../controller/auth.controller.js'
+import { sign_up, login, refresh } from '../controller/auth.controller.js'
 
 const router = express.Router()
 
 router.post('/sign_up', sign_up);
+router.post('/login', login);
+router.post('/refresh', refresh);
 
 export default router;
